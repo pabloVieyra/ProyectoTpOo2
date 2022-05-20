605b0d396eafec47c5c752d7d4714b875539ca9f
sudava10@gmail.com <Sudava10@gmail.com>
2022-05-20 18:28:47 -0300

    https://c4-g23-t.atlassian.net/browse/JAVA-9 #2 Estructura del proyecto Login con roles y encriptacion de contraseña

---
+++ src/main/java/com/ProyectoTpOo2/appG5/helpers/ViewRouteHelper.java
@@ -0,0 +1,14 @@
+package com.ProyectoTpOo2.appG5.helpers;
+
+public class ViewRouteHelper {
+
+	public final static String INDEX = "index";
+	//CAMBIAR NOMBRE
+	public final static String RAIZ = " ";
+	public final static String MENU = "menu";
+	public final static String ADMIN = "admin";
+	public final static String MANAGER = "manager";
+	public final static String PROFESSOR = "professor";
+}
+
+
