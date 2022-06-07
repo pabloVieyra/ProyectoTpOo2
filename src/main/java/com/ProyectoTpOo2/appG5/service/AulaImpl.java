bde106eb91a6851cdc8c438cd402a8b67bd622a2
NickFNS <fsierra7180@gmail.com>
2022-06-07 01:33:39 -0300

    -Creacion de repositorios para comunicacion con la base de datos de ciertas entidades
    -Logica de paquetes service (Aula y User)

---
+++ src/main/java/com/ProyectoTpOo2/appG5/service/AulaImpl.java
@@ -0,0 +1,38 @@
+package com.ProyectoTpOo2.appG5.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ProyectoTpOo2.appG5.entity.Aula;
+import com.ProyectoTpOo2.appG5.entity.Edificio;
+
+@Service
+@Transactional
+public class AulaImpl implements AulaService {
+	@Autowired
+	AulaRepository repository;
+
+	@Autowired
+	EspacioRepository espacioRepository;
+
+	public List<Aula> getAllAula() {
+		return (List<Aula>) repository.findAll();
+	}
+
+	public List<Aula> traerPorEdificio(Edificio edificio) {
+		return (List<Aula>) repository.findByEdificio(edificio);
+	}
+
+	public Aula getAula(int numAula) {
+		return repository.findByNumAula(numAula);
+	}
+
+	@Override
+	public List<Aula> traerPorEdificioEnOrden() {
+		return repository.findAllByOrderByEdificioAsc();
+	}
+}
