1e8d7ee4eb4112c6dbe07894f9381f813e28baec
Emanuel Sotelo <emanicosotelo@gmail.com>
2022-06-07 02:11:37 -0300

    Creación de service final y modificacion paquete seguidad.

--- src/main/java/com/ProyectoTpOo2/appG5/service/FinalService.java
+++ src/main/java/com/ProyectoTpOo2/appG5/service/FinalService.java
@@ -1,9 +1,16 @@
 package com.ProyectoTpOo2.appG5.service;
 
+import java.util.Optional;
+
 import com.ProyectoTpOo2.appG5.entity.Final;
 
 public interface FinalService{
 
 	public Iterable<Final> getAllFinales();
 	
+	public Final crearFinal(Final fin);
+	
+	public Optional<Final> traerFinalId(Long id);
+	
+	public void borrarFinal(Long id) throws Exception;
 }
